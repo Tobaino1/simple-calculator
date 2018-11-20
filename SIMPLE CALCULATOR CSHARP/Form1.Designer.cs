a81f03ebac5c93436b8cb654329da6dcbcbc4d50
Tobaino1 <tobainocycle@yahoo.com>
2018-11-20 11:55:02 +0100

    modification

--- SIMPLE CALCULATOR CSHARP/Form1.Designer.cs
+++ SIMPLE CALCULATOR CSHARP/Form1.Designer.cs
@@ -43,10 +43,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(231, 40);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(184, 56);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.Size = new System.Drawing.Size(196, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "SIMPLE CALCULATOR";
             // 
